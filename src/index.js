0e7294d01b9d64f5c79d880e6ede142bce054e3b
MuriloPereira <murilopereiraprado0@gmail.com>
2024-01-17 19:25:20 -0300

    Node básico usando nodemon, e com algumas telas simples

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ const app = express()
 const router = express.Router()
 
 router.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname + "/pages/home.html"))
-})
+    res.sendFile(__dirname + "/pages/home.html");
+});
 
-router.get("/contato", (req,res)=>{
-    res.sendFile(path.join(__dirname + "/pages/contato.html"))
-})
+router.get("/sobre", (req,res)=>{
+    res.sendFile(__dirname + "/pages/sobre.html");
+});
 
-app.use(router)
+router.get("/blog", function(req,res){
+    res.send("bem-vindo ao blog");
+});
+
+router.get("/ola/:nome/:cargo", function(req,res){
+    res.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2>seu cargo é : " + req.params.cargo + "</h2>");
+});
+
+app.use(router);
 
 app.listen(3333, ()=>{
-    console.log("servidor rodando")
-})
+    console.log("servidor rodando na url http://localhost:3333");
+});
